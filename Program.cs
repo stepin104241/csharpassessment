751e6aa44a9ed157e1fe4ab53bdb484ce6b020bb
Roshni Poovanna C <69103041+stepin104241@users.noreply.github.com>
2020-10-03 16:04:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class Customer
+    {
+        public Customer(int customerid, string name, string address, int phone)
+        {
+            Customerid = customerid;
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+
+        public int Customerid { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Phone { get; set; }
+    }
+    interface ICustomerManager
+    {
+        bool AddCustomer(Customer cs);
+        bool DeleteCustomer(int customerid);
+        bool UpdateCustomer(int id);
+        bool ViewAllCustomers();
+    }
+    class 
+        CustomerManager : ICustomerManager
+    {
+        HashSet<Customer> customers = new HashSet<Customer>();
+        public bool AddCustomer(Customer cs)
+        {
+            return customers.Add(cs);
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            foreach (Customer cs in customers)
+            {
+                if (cs.Customerid == id)
+                {
+                    customers.Remove(cs);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        
+
+        public bool UpdateCustomer(int id)
+        {
+            foreach (Customer cs in customers)
+            {
+                if (cs.Customerid == id)
+                {
+                    Console.Write("Enter the New Title: ");
+                    string newname = Console.ReadLine();
+                    Console.Write("Enter the New Author: ");
+                    string newaddress = Console.ReadLine();
+                    Console.Write("Enter the New Price: ");
+                    double newphone = double.Parse(Console.ReadLine());
+                    cs.Name = newname;
+                    cs.Address = newaddress;
+                    cs.Phone = newphone;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool ViewAllCustomers()
+        {
+            foreach (var cs in customers)
+            {
+                Console.WriteLine("-------------------------------");
+                Console.WriteLine($"Customer ID: {cs.Customerid}");
+                Console.WriteLine($"Name: {cs.Name}");
+                Console.WriteLine($"Address: {cs.Address}");
+                Console.WriteLine($"Phone: {cs.Phone}");
+                Console.WriteLine("-------------------------------");
+            }
+            return true;
+        }
+    }
+
+    class CollectionCustomerManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the details of 1st customer");
+            Customer c1 = Console.ReadLine();
+            Console.WriteLine("enter the details of 2nd customer");
+            Customer c2 = Console.ReadLine();
+            Console.WriteLine("enter the  details of 3rd customer");
+            Customer c3 = Console.ReadLine();
+           
+            CustomerManager cus = new CustomerManager();
+            cus.AddCustomer(c1);
+            cus.AddCustomer(c2);
+            cus.AddCustomer(c3);
+            cus.ViewAllCustomers();
+            cus.UpdateCustomer(100);
+            cus.ViewAllCustomers();
+         
+        }       
+    }
+}
---
+++ Serialization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public int RollNo { get; set; }
+        public string Address { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"Name: {Name}\nRoll No: {RollNo}\nAddress: {Address}");
+        }
+    }
+    class BinarySerializationDemo
+    {
+        static void Main(string[] args)
+        {
+            BinarySerialization();
+        }
+        private static void BinarySerialization()
+        {
+            Console.Write("Read or Write? (Read/Write): ");
+            string choice = Console.ReadLine();
+            if (choice.ToLower() == "read")
+                deserialize();
+            else
+                serialize();
+        }
+
+        private static void serialize()
+        {
+            Student s = new Student { Name = "Roshni", RollNo = 45, Address = "Mysore" };
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream fs = new FileStream("Demo.bin", FileMode.OpenOrCreate, FileAccess.Write);
+            bf.Serialize(fs, s);
+            fs.Close();
+        }
+
+        private static void deserialize()
+        {
+            FileStream fs = new FileStream("Demo.bin", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            Student s = bf.Deserialize(fs) as Student;
+            Console.WriteLine(s.Name);
+            Console.WriteLine(s.RollNo);
+            Console.WriteLine(s.Address);
+            fs.Close();
+        }
+    }
+}
+
